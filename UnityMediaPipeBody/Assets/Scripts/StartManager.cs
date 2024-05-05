97f2e2b8768cf927489a8f6880b3affd37ee5719
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2024-05-06 00:56:31 +0900

    🔨 Feat | StartScene에서 GameData Load
    시작화면(StartScene)이 추가되면서 가장 처음 화면인 시작 화면에서 GameData load해줌

--- UnityMediaPipeBody/Assets/Scripts/StartManager.cs
+++ UnityMediaPipeBody/Assets/Scripts/StartManager.cs
@@ -8,7 +8,7 @@ public class StartManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // GameData.instance.LoadMonsterData();
+        GameData.instance.LoadMonsterData();
     }
 
     // Update is called once per frame
