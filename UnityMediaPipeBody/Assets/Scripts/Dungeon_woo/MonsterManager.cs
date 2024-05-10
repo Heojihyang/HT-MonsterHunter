2762c18d73807ba800b64749a1ec0dc611e22284
woojeong <77192050+wooj22@users.noreply.github.com>
2024-05-11 01:53:53 +0900

    💚 Add | 운동 모형 라인렌더러 테스트 씬, 스크립트 추가

--- UnityMediaPipeBody/Assets/Scripts/Dungeon_woo/MonsterManager.cs
+++ UnityMediaPipeBody/Assets/Scripts/Dungeon_woo/MonsterManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MonsterManager : MonoBehaviour
 {
-    /*
     //몬스터 10종
     public GameObject[] Monsters = new GameObject[10];
 
@@ -16,15 +15,6 @@ public class MonsterManager : MonoBehaviour
     // 각 던전에 맞는 몬스터 생성
     private void InitializeMonsters()
     {
-        if (monsterIndex >= 0 && monsterIndex < Monsters.Length)
-        {
-            // 선택한 몬스터 인덱스에 해당하는 오브젝트 생성
-            Instantiate(Monsters[monsterIndex], transform.position, Quaternion.identity);
-        }
-        else
-        {
-            Debug.LogError("Invalid monster index: " + monsterIndex);
-        }
+
     }
-    */
 }
