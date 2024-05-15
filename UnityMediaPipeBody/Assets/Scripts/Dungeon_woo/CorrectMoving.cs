777f1598220510dbdbdcfe385fb5cc7fa54c785b
woojeong <77192050+wooj22@users.noreply.github.com>
2024-05-16 02:34:21 +0900

    🔨 Feat | 모델 중심 고정
    - 플레이어 모델 카메라 보정 ON
    - 정답모델과 몬스터 모두 카메라 상속

--- UnityMediaPipeBody/Assets/Scripts/Dungeon_woo/CorrectMoving.cs
+++ UnityMediaPipeBody/Assets/Scripts/Dungeon_woo/CorrectMoving.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CorrectMoving : MonoBehaviour
 {
+    public Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.SetParent(mainCamera.transform, false);
+        Debug.Log("카메라(부모) - 정답모델(자식) 관계 설정 성공");
     }
 
     // Update is called once per frame
--- UnityMediaPipeBody/Assets/Scripts/Dungeon_woo/MonsterController.cs
+++ UnityMediaPipeBody/Assets/Scripts/Dungeon_woo/MonsterController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 // 몬스터 프리팹에 적용
 public class MonsterController : MonoBehaviour
 {
+    public Camera mainCamera;
+
     public GameObject[] Monsters = new GameObject[10];  // 몬스터 10종 프리팹
     public GameObject monster;                          // 생성된 몬스터
     public Animator animator;                           // 생성된 몬스터의 애니메이터
@@ -15,6 +17,9 @@ public class MonsterController : MonoBehaviour
 
     private void Start()
     {
+        transform.SetParent(mainCamera.transform, false);
+        Debug.Log("카메라(부모) - 몬스터(자식) 관계 설정 성공");
+
         int monNum = dungeonScene.GetComponent<TDungeonSceneManager>().receivedMonsterNumber;
         CreateMonster(monNum);                          // 몬스터 생성
         currentHealth = maxHealth;                      // 체력 초기화
