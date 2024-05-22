ad65f040b6359988ddb2dd111d6dd98a6d11c8f6
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2024-05-23 00:12:06 +0900

    🔨 Feat | 운동 부위 선택 UI 추가
    - MonsterData 인덱스에 알맞게 각 부위별 UI 수정
    - 운동 부위 10개의 몬스터 추가

--- UnityMediaPipeBody/Assets/Scripts/SelectManager.cs
+++ UnityMediaPipeBody/Assets/Scripts/SelectManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class SelectManager : MonoBehaviour
 {
     public GameObject Explain;
+    // public GameObject Blob;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +20,26 @@ public class SelectManager : MonoBehaviour
         
     }
 
-    // (허벅지) 클릭 시 : 운동설명 창 활성화
+    public void ActBlob()
+    {
+        // Blob.SetActive(false);
+    }
+
+
+    // 버튼 클릭 시 : 운동설명 창 활성화
     public void ActExerciseExplain()
     {
         Explain.SetActive(true);
+        
+    }
+
+    // 운동 설명 창에서 뒤로가기 클릭 시 : 운동설명 창 비활성화
+    public void InActExerciseExplain()
+    {
+        Explain.SetActive(false);
+
+        // 켜져있던 몬스터 off 
+        // Blob.SetActive(true);
     }
 
     // (시작) 클릭 시 : 던전으로 이동
@@ -30,5 +47,11 @@ public class SelectManager : MonoBehaviour
     {
         SceneManager.LoadScene("TDungeonScene");    
         PlayerPrefs.SetInt("MonsterNumberToSend", 0);   //씬 로드하면서 몬스터 번호 같이 넘겨주기
+    }
+
+    // (시작) 클릭 시 : 메인 화면으로 이동
+    public void ChangeMainScene()
+    {
+        SceneManager.LoadScene("TMainScene");
     }
 }
